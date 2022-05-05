6d461f80fb9cc27f28b8aa40d2dc93ef48e53a13
cla.junior21@gmail.com <cla.junior21@gmail.com>
2022-05-05 14:51:23 -0300

    Aula 2

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,14 +1,17 @@
 import { Popover } from '@headlessui/react';
 
 import { ChatTeardropDots } from 'phosphor-react';
+import { WidgetForm } from './WidgetForm/Index';
 
 export function Widget(){
 
 
     return(
-        <Popover className="absolute bottom-5 right-5 ">
+        <Popover className="absolute bottom-4 right-4 md:bottom-10 md:right-10 flex flex-col align-end">
 
-            <Popover.Panel>Hello World :)</Popover.Panel>
+            <Popover.Panel>
+                <WidgetForm />
+            </Popover.Panel>
 
             <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group " >
                 <ChatTeardropDots className="w-6 h-6" />
